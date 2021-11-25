cbca107171adfd6462908bb0aeb6430e4397bc29
zigatrcek <ziga.trc@gmail.com>
2021-11-25 18:53:15 +0100

    puši kurac kreten to sploh ni res, upam da crkneš

--- examples/17-game/17-game.js
+++ examples/17-game/17-game.js
@@ -7,7 +7,11 @@ import { Physics } from './Physics.js';
 import { Camera } from './Camera.js';
 import { SceneLoader } from './SceneLoader.js';
 import { SceneBuilder } from './SceneBuilder.js';
+import { Mesh } from './Mesh.js';
+import { Model } from './Model.js';
+
 import { update_wave } from './UpdateWave.js';
+
 // import Helper from './Helper.js';
 
 class App extends Application {
@@ -19,79 +23,140 @@ class App extends Application {
         this.time = Date.now();
         this.startTime = this.time;
         this.aspect = 1;
-
+        
         this.pointerlockchangeHandler = this.pointerlockchangeHandler.bind(this);
         document.addEventListener('pointerlockchange', this.pointerlockchangeHandler);
-
+        
         this.load('scene.json');
     }
-
+    
     async load(uri) {
-        const scene = await new SceneLoader().loadScene(uri);
-        const builder = new SceneBuilder(scene);
-        this.scene = builder.build();
+        this.spec = await new SceneLoader().loadScene(uri);
+        this.builder = new SceneBuilder(this.spec);
+        this.scene = this.builder.build();
         this.physics = new Physics(this.scene);
         // this.helper = new Helper(this.scene, scene);
-        // this.test = false;
+        this.paused = false;
         this.waypoints = [[0, 1], [10, 1], [10, 5], [6, 5], [6, 3], [1, 3], [1, 10], [8, 10], [8, 8], [11, 8]];
-        
-
-        this.spawnWave(this.scene, builder, [
-        {
-          "type": "model",
-          "mesh": 0,
-          "texture": 2,
-          "aabb": {
-            "min": [-1, -1, -1],
-            "max": [1, 1, 1]
-          },
-          "translation": [0, -50, 0],
-          "scale": [0.25, 0.25, 0.25],
-        },{
-            "type": "wait"
-        },
-        {
+        this.currentWave = 0;
+        this.enemyTypes = [
+            {
+                "type": "wait"
+            },
+            {
             "type": "model",
             "mesh": 0,
-            "texture": 2,
+            "texture": 3,
             "aabb": {
-              "min": [-1, -1, -1],
-              "max": [1, 1, 1]
+                "min": [-1, -1, -1],
+                "max": [1, 1, 1]
             },
-            "translation": [0, -50, 0],
+            "translation": [0, 3, 0],
             "scale": [0.25, 0.25, 0.25],
-          }
-    ]);
-
-        // Find first camera.
-        this.camera = null;
-        this.scene.traverse(node => {
-            if (node instanceof Camera) {
-                this.camera = node;
+        },
+        ]
+        this.waves = [
+            [1],
+            [1, 0, 1, 1, 0, 0, 1, 1, 1, 1],
+            [1, 0, 1, 1, 0, 0, 1, 1, 1, 1],
+            [1, 0, 1, 1, 0, 0, 1, 1, 1, 1],
+        ]
+        this.turretTypes = [
+            {
+                "type": "model",
+                "mesh": 2,
+                "texture": 2,
+                "aabb": {
+                    "min": [-0.2, -1, -0.2],
+                    "max": [0.2, 1, 0.2]
+                },
+              "translation": [0, 0.5, 0],
+              "scale": [0.03, 0.03, 0.03]
             }
-        });
+        ]
+        this.turretGrid = JSON.parse(JSON.stringify(this.spec.map.grid));
+        console.log(this.turretGrid);
+        for (let y in this.turretGrid) {
+            for (let x in this.turretGrid) {
+                if (this.turretGrid[y][x] == 1) {
+                    this.turretGrid[y][x] = 'X';
+                }
+            }
+        }
+        console.log(this.turretGrid);
 
+// Find first camera.
+this.camera = null;
+this.scene.traverse(node => {
+    if (node instanceof Camera) {
+        this.camera = node;
+    }
+});
         this.camera.aspect = this.aspect;
         this.camera.updateProjection();
         this.renderer.prepare(this.scene);
     }
 
-    spawnWave(scene, builder, wave) {
+    spawnWave(wave) {
         let i = 1;
         let distance = 4; //distance_traveled between enemies, could also add delay object to the model
-        for(let enemy of wave) {
+        console.log(wave);
+        for(let enemyIndex of wave) {
+            let enemy = this.enemyTypes[enemyIndex];
             //there's now also type "wait" to increment i and increase distance between objects
             if (enemy.type == "model"){
-                let model = builder.createNode(enemy);
+                let model = this.builder.createNode(enemy);
                 model.id = i;
                 model.distance_traveled = 0 - (i * distance);
-                scene.addNode(model);
-                console.log(model);
+                model.role = "enemy";
+                this.scene.addNode(model);
+                // console.log(model);
             }
             i++;
         }
-        console.log("this scene:");
-        console.log(this.scene);
+        this.renderer.prepare(this.scene);
+        
+    }
+
+    spawnNextWave() {
+        if(this.currentWave >= this.waves.length) return;
+        
+        this.spawnWave(this.waves[this.currentWave]);
+        this.currentWave++;
+    }
+    
+    spawnTurret(turretIndex, position){
+        let turret = this.turretTypes[turretIndex];
+        
+        
+        const mesh = new Mesh(this.spec.meshes[turret.mesh]);
+        const texture = this.spec.textures[turret.texture];
+        let model = new Model(mesh, texture, turret);
+
+
+        let x = model.translation[0];
+        let y = model.translation[1];
+        let z = model.translation[2];
+        
+        //console.log(position);
+        //console.log(position[1]);
+        
+        model.translation = [position[0], y, position[1]];
+        
+        model.updateTransform();
+        model.role = "turret";
+        model.target = -1;
+        
+        this.scene.addNode(model);
+        this.turretGrid[position[1]][position[0]] = 'X';
+    }
+    // h>
+    spawnTurret1(){
+        this.spawnTurret(0, [8, 3]);
+        this.spawnTurret(0, [5, 5]);
+        this.spawnTurret(0, [7, 8]);
+        this.spawnTurret(0, [10, 10]);
+        this.renderer.prepare(this.scene); 
     }
 
     enableCamera() {
@@ -122,7 +187,9 @@ class App extends Application {
         if (this.physics) {
             this.physics.update(dt);
         }
-        update_wave(this.scene, this.waypoints);
+        if (!this.paused) {
+            update_wave(this.scene, this.waypoints);
+        }
         // this.update_game()
 
         // if (this.helper != null && !this.test) {
@@ -161,13 +228,21 @@ class App extends Application {
 		return temp;
 	}
 
+    pause_unpause() {
+        this.paused = !this.paused;
+    }
+
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
     const canvas = document.querySelector('canvas');
     const app = new App(canvas);
     const gui = new GUI();
     gui.add(app, 'enableCamera');
+    gui.add(app, 'spawnNextWave');
+    gui.add(app, 'spawnTurret1');
+    gui.add(app, 'pause_unpause');
 });
 
 
--- examples/17-game/UpdateWave.js
+++ examples/17-game/UpdateWave.js
@@ -1,10 +1,19 @@
 // Run on every frame, if the wave is active. If we are between waves or the game is paused, this function is not called.
 export function update_wave(scene, path) {
     traverse_paths(scene, path);
+    update_turrets(scene, path);
 }
 
-function update_turrets() {
-    // locking, rotation, shooting
+function update_turrets(scene, path) {
+    if (scene != null){
+        for (let enemy of scene.nodes){
+            if (enemy != null) {
+                if (enemy.role == "turret"){
+                    update_turret(scene, enemy, path);
+                }   
+            }
+        }
+    }
 }
 
 function update_bullets() {
@@ -15,6 +24,108 @@ function update_enemies() {
     // TODO update enemies
         // movement, hp calculation
 
+}
+
+function distance(pos1, pos2) {
+    return Math.sqrt(Math.pow(pos1[0] - pos2[0], 2) + Math.pow(pos1[2] - pos2[2], 2));
+}
+
+
+function update_turret(scene, turret, path) {
+    let RANGE = 5;
+
+    let target = findEnemyById(scene, turret.target);
+    
+    if (target == null || turret.target == -1) {
+        target = find_target(scene, turret, path, RANGE);
+    }
+
+
+    
+    //console.log("target")
+    //console.log(target);
+
+    if (target == null) {
+        return;
+    }
+
+    
+    let turretLocation = [turret.translation[0], turret.translation[2]];
+    let targetLocation = [target.translation[0], target.translation[2]];
+    if (targetLocation[0] == -100 || targetLocation[1] == -100) {
+        turret.target = -1;
+        return;
+    }
+    //console.log(turretLocation + " -> " + targetLocation);
+    let Y = turretLocation[1] - targetLocation[1];
+    let X = turretLocation[0] - targetLocation[0];
+    let vec = [X, Y];
+    let norm = vectorNorm(vec);
+    let fi = 0;
+    if (Y == 0){
+
+    } else {
+        fi = Math.acos(X / norm) * (Y / Math.abs(Y));
+    }
+    
+    let start = 0;
+    fi = fi + start;
+    turret.rotation[1] = -Math.PI / 2 - fi;
+    turret.updateTransform();    
+}
+
+function findEnemyById(scene, id){
+    if (id == -1) return;
+    if (scene != null){
+        for (let enemy of scene.nodes){
+            if (enemy != null) {
+                if (enemy.id = id){
+                    return enemy;
+                }   
+            }
+        }
+    }
+    return;
+}
+
+function vectorNorm(v){
+    return Math.sqrt(scalarProd(v, v));
+}
+
+function scalarProd(v1, v2){
+
+    if(v1.length != v2.length){
+        console.log("ERROR");
+        return -1;
+    }
+    let v = 0;
+    for(let i = 0; i < v1.length; i++){
+        v += v1[i]*v2[i];
+    }
+    return v;
+
+}
+
+
+function find_target(scene, turret, path, range) {
+    if (scene != null){
+        let best_dist = 2000000;
+        let best = null;
+        for (let enemy of scene.nodes){
+            if (enemy != null) {
+                if (enemy.role == "enemy"){
+                    let dist = distance(turret.translation, enemy.translation);
+                    if (dist <= range && dist < best_dist){
+                        //console.log(dist);
+                        best = enemy;
+                        best_dist = dist;
+                    }
+                }   
+            }
+        }
+        return best;
+    }
+    return;
 }
 
 function get_path_position(distance_to_travel, path) {
@@ -48,7 +159,7 @@ function get_path_position(distance_to_travel, path) {
 function traverse_path(enemy, path){
 
     //speed = get_path_position()
-    let move = 0.02;
+    let move = 0.05;
     let res = get_path_position(enemy.distance_traveled + move, path);
     enemy.distance_traveled += move;
 
